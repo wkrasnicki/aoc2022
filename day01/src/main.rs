86b6ec7baa7f274b633dc3be31c099b02f0bb20b
Wiktor Kraśnicki <wikkra@gmail.com>
2022-12-02 19:35:54 +0100

    Day01a solution

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./input.txt").expect("Could not read file");
+    let reader = BufReader::new(file);
+
+    let result = reader
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>()
+        .split(|line| line.trim().is_empty())
+        .map(|items| {
+            items
+                .iter()
+                .map(|item| item.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .max()
+        .unwrap_or(0);
+
+    println!("{}", result);
 }
