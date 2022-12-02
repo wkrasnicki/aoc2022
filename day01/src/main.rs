63e5736bbeafb53bf2a976cd907dbb01882089cb
Wiktor Kraśnicki <wikkra@gmail.com>
2022-12-02 20:26:29 +0100

    Day01b solution

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,11 +1,15 @@
+use std::collections::BinaryHeap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::iter;
 
 fn main() {
     let file = File::open("./input.txt").expect("Could not read file");
     let reader = BufReader::new(file);
 
-    let result = reader
+    let n = 3;
+
+    let mut elves = reader
         .lines()
         .map(|line| line.unwrap())
         .collect::<Vec<String>>()
@@ -16,8 +20,11 @@ fn main() {
                 .map(|item| item.parse::<i32>().unwrap())
                 .sum::<i32>()
         })
-        .max()
-        .unwrap_or(0);
+        .collect::<BinaryHeap<i32>>();
 
-    println!("{}", result);
+    println!("{}", elves.peek().unwrap_or(&0));
+
+    let top_elves = iter::repeat(()).take(n).map(|_| elves.pop().unwrap_or(0));
+
+    println!("{}", top_elves.sum::<i32>());
 }
